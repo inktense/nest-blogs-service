4a044379f19b18ee4170150a0adb30d0710543b9
Bianca Popescu <inktense12@gmail.com>
2025-11-16 15:12:09 +0000

    feat: Added change for future type mapping

---
+++ src/common/entities/blog.entity.ts
@@ -0,0 +1,7 @@
+export class Blog {
+  id: string;
+  name: string;
+  slug: string;
+  created_at: string;
+  updated_at: string;
+}
---
+++ src/common/entities/post.entity.ts
@@ -0,0 +1,8 @@
+export class Post {
+  id: string;
+  blog: string;
+  title?: string;
+  content: string;
+  created_at: string;
+  updated_at: string;
+}
